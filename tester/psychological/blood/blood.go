0b87b47337a16cf749dd42218846526422385975
kanguk2 <kang_woog@naver.com>
2023-04-09 11:04:45 +0900

    feat : 두 문자를 정렬하는 함수를 구현 (#1)
    * feat : 두 문자를 정렬하는 함수를 구현
    
    * Refactor
    
    ---------
    
    Co-authored-by: kevinkanguk <kulee@huinno.com>
    Co-authored-by: horace-velmont <dragond7@naver.com>

---
+++ tester/psychological/blood/blood.go
@@ -0,0 +1,40 @@
+package blood
+
+import (
+	"errors"
+	"sort"
+)
+
+// GetBloodType 두 문자 를 정렬하여 BloodType 정보를 리턴한다.
+func GetBloodType(leftChromosome string, rightChromosome string) (BloodType, error) {
+	chromosome := []string{leftChromosome, rightChromosome}
+	sort.Strings(chromosome)
+
+	if chromosome[0] == "O" && chromosome[1] == "O" {
+		return O, nil
+	}
+	if chromosome[0] == "A" {
+		if chromosome[1] == "B" {
+			return AB, nil
+		}
+		if chromosome[1] == "A " || chromosome[1] == "O" {
+			return A, nil
+		}
+	}
+	if chromosome[0] == "B" {
+		if chromosome[1] == "B " || chromosome[1] == "O" {
+			return B, nil
+		}
+	}
+
+	return "", errors.New("can't find blood type")
+}
+
+type BloodType string
+
+const (
+	A  BloodType = "A"
+	B  BloodType = "B"
+	AB BloodType = "AB"
+	O  BloodType = "O"
+)
